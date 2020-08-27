a88f61e06efa2858d9f825f70e47fafe1667b0dd
Cho Won Jae <yoteamo7@naver.com>
2020-08-27 19:22:48 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,36 +20,28 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-    id : 1,
-    image : 'https://placeimg.com/64/64/any',
-    name : '홍길동',
-    birthday : 970924,
-    gender : '남자',
-    job : '학생'
-  },
-  {
-    id : 2,
-    image : 'https://placeimg.com/64/64/2',
-    name : '김철수',
-    birthday : 950420,
-    gender : '남자',
-    job : '프로그래머'
-  },
-  {
-    id : 3,
-    image : 'https://placeimg.com/64/64/3',
-    name : '이순신',
-    birthday : 910212,
-    gender : '남자',
-    job : '데이터분석가'
-  }
-]
+
 
 class App extends React.Component {
+  state = {
+    customers : ""
+  }
+
+componentDidMount(){
+  this.callApi()
+    .then(res => this.setState({customers : res}))
+    .catch(err => console.log(err));
+}
+
+callApi = async () => {
+  const response = await fetch('/api/customers');
+  const body = await response.json();
+  return body;
+}
+
   render(){
     const { classes } = this.props;
+    const { customers } = this.state;
     return ( 
       <Paper className={classes.root}>
         <Table className={classes.table}>
@@ -64,7 +56,7 @@ class App extends React.Component {
             </TableRow>
           </TableHead>
           <TableBody>
-                {customers.map(customer => 
+                {customers ? customers.map(customer =>   //async 이기 때문에 customers가 비어있을 수 있으므로 삼항연산자를 사용!
                 <Customer 
                   key={customer.id}
                   id={customer.id}
@@ -74,7 +66,7 @@ class App extends React.Component {
                   gender={customer.gender}
                   job={customer.job}
                 />
-              )}
+              ) : ""} 
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const bodyParser = require('body-parser');
 app.use(express.json()); 
 app.use(express.urlencoded( {extended : false } ));
 
-app.get('/api/hello', (req, res)=>{
-    res.send({message : 'Hello Express!'});
+app.get('/api/customers', (req, res)=>{ 
+    res.send([  //배열 데이터는 json 형식으로 반환
+        {
+          id : 1,
+          image : 'https://placeimg.com/64/64/any',
+          name : '홍길동',
+          birthday : 970924,
+          gender : '남자',
+          job : '학생'
+        },
+        {
+          id : 2,
+          image : 'https://placeimg.com/64/64/2',
+          name : '김철수',
+          birthday : 950420,
+          gender : '남자',
+          job : '프로그래머'
+        },
+        {
+          id : 3,
+          image : 'https://placeimg.com/64/64/3',
+          name : '이순신',
+          birthday : 910212,
+          gender : '남자',
+          job : '데이터분석가'
+        }
+      ]);
 })
 
 app.listen(port, ()=>{
